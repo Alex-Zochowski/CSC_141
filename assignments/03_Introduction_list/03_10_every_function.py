fa69af5a39a18c604f491cf2ad671954898b7a69
Alex-Zochowski <alex.zochowski001@albright.edu>
2026-09-15 12:32:28 -0400

    up

--- assignments/03_Introduction_list/03_10_every_function.py
+++ assignments/03_Introduction_list/03_10_every_function.py
@@ -1,10 +1,13 @@
-random = ["Mount Fuji", "Nile River", "Itally", "New York City", "German"]
+random = ["Mount Fuji", "Nile River", "Italy", "New York City", "German"]
 print(random)
 print(random[3])
+random.remove("Italy")
 print(sorted(random))
 random.reverse()
+random[2] = "apple"
 print(random)
 random.reverse()
 print(sorted(random, reverse=True))
+random.insert(2, "Italy")
 length = len(random)
 print(length)
